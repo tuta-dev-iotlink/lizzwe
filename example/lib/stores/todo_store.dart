9afb3a27aec8a96d2bf1934ed71f0c87dac930ee
tuta-dev-iotlink <ichimoku.0902@gmail.com>
2025-04-28 16:35:51 +0700

    feat: clear logic key

--- example/lib/stores/todo_store.dart
+++ example/lib/stores/todo_store.dart
@@ -2,33 +2,63 @@ import 'package:lizzwe/lizzwe.dart';
 import '../models/todo.dart';
 
 class TodoStore {
-  final StateStore _store = StateStore();
-  late final Observable<List<Todo>> _todos;
+  final _store = StateStore();
 
-  TodoStore() {
-    _todos = _store.create('todos', <Todo>[]);
+  // Observable states
+  late final todos = _store.create<List<Todo>>([]);
+  late final isLoading = _store.create<bool>(false);
+  late final error = _store.create<Object?>(null);
+
+  Future<void> addTodo(String title) async {
+    try {
+      isLoading.value = true;
+      error.value = null;
+
+      final newTodo = Todo(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        title: title,
+        isCompleted: false,
+      );
+      todos.value = [...todos.value, newTodo];
+    } catch (e) {
+      error.value = e;
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  Observable<List<Todo>> get todos => _todos;
+  Future<void> toggleTodo(String id) async {
+    try {
+      isLoading.value = true;
+      error.value = null;
 
-  void addTodo(String title) {
-    final newTodo = Todo(
-      id: DateTime.now().millisecondsSinceEpoch.toString(),
-      title: title,
-    );
-    _todos.value = [..._todos.value, newTodo];
+      todos.value = todos.value.map((todo) {
+        if (todo.id == id) {
+          return todo.copyWith(isCompleted: !todo.isCompleted);
+        }
+        return todo;
+      }).toList();
+    } catch (e) {
+      error.value = e;
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  void toggleTodo(String id) {
-    _todos.value = _todos.value.map((todo) {
-      if (todo.id == id) {
-        return todo.copyWith(isCompleted: !todo.isCompleted);
-      }
-      return todo;
-    }).toList();
+  Future<void> deleteTodo(String id) async {
+    try {
+      isLoading.value = true;
+      error.value = null;
+
+      todos.value = todos.value.where((todo) => todo.id != id).toList();
+    } catch (e) {
+      error.value = e;
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  void deleteTodo(String id) {
-    _todos.value = _todos.value.where((todo) => todo.id != id).toList();
+  void dispose() {
+    _store.clear();
   }
 }
